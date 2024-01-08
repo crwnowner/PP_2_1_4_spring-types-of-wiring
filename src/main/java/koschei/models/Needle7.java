6b2567882bd10c17992c6238830ea58cf8ae9a23
crwnowner <8maxabb1122@mail.ru>
2024-01-08 12:24:50 +0300

    done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
 public class Needle7 {
+    private Deth8 death;
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + death.toString();
     }
 }
